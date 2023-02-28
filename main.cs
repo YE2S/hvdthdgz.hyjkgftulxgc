4ca55ae1d5840a05bfe609e31266e1dcd751e0ac
YE2S <mamiksxxxxxx@gmail.com>
2023-02-28 07:27:36 +0300

    Now hello

--- main.cs
+++ main.cs
@@ -1,10 +1,15 @@
+using System;
+
 namecpace Git
 {
 class Git
 {
 static void Main(string[] args)
 {
-System.Console.Writeline("Hello Git!");
+System.Console.WriteLine("Enter your name: ");
+string name = Console.ReadLine();
+System.Console.WriteLine(name.Lenght);
+System.Console.WriteLine($"Hello (name)!");
 }
 }
 }
